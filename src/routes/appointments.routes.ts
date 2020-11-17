6fd7d59b7c79a46fba1c7e0c3efdf4621472229c
Taynara Pecorario <taynara_rp22@hotmail.com>
2020-11-16 22:13:30 -0300

    solid e services

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,28 +1,37 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
+import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body; // provider: nome do profissional / date: data do agendamento
+  try {
+    const { provider, date } = request.body;
 
-  const parsedDate = startOfHour(parseISO(date)); //  arredonda a hora pro começo dela, ex: 15h54 vira 15h
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(
-    parsedDate,
-  );
+    const parsedDate = parseISO(date);
 
-  if (findAppointmentInSameDate) {
-    return response
-      .status(400)
-      .json({ message: 'This appointment is already booked' });
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+
+    const appointment = createAppointment.execute({
+      date: parsedDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create(provider, parsedDate);
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
